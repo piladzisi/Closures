7f928bc5b51f10cb5d7e866cb23ef07b8bb472c2
Anna <piladzisi@gmail.com>
2019-04-15 18:39:49 +0300

    first commit

---
+++ Closures.playground/Contents.swift
@@ -0,0 +1,91 @@
+func printString(_ string: String) {
+    print("Printing the string passed in as an argument \(string)")
+}
+
+printString("Hi, my name is Robot")
+
+let stringPrinterFunction = printString
+stringPrinterFunction("Hi, again")
+
+func sum(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+let addTwoNumbers = sum
+
+addTwoNumbers(2,5)
+
+//Functions as Parameters
+func displayString(usingFunction function: (String) -> Void) {
+    function("I'm a function inside a function")
+}
+displayString(usingFunction: printString)
+
+//challenge
+func addTwo(to: Int) -> Int {
+    return to + 2
+}
+addTwo(to: 5)
+let addition = addTwo
+let result = addition(9)
+
+
+extension Int {
+    func applyOperation (_ operation: (Int) -> Int) -> Int {
+        return operation(self)
+    }
+}
+
+func double(_ value: Int) -> Int {
+    return value * 2
+}
+10.applyOperation(double)
+
+func closestMultipleOfSix(_ value: Int) -> Int {
+    for x in 1...100 {
+        let multiple = x * 6
+        
+        if multiple - value < 6 && multiple > value {
+            return multiple
+        } else if multiple == value {
+            return value
+        }
+    }
+    return 0
+}
+
+32.applyOperation(closestMultipleOfSix)
+12.applyOperation(closestMultipleOfSix)
+200.applyOperation(closestMultipleOfSix)
+
+// capturing variables
+func gameCounter() -> ((Int) -> Void ){
+    func increment(_ i: Int) {
+        print("Integer passed in: \(i)")
+    }
+    return increment
+}
+let counter = gameCounter()
+counter(1)
+
+extension String{
+    
+    func transform(_ argument : (String) -> String) -> String {
+        return  argument(self)
+    }
+}
+
+func removeVowels(from value: String) -> String {
+    var output = ""
+    
+    for char in value{
+        if !(char == "a" || char == "A" || char == "e" || char == "e"
+            || char == "i" || char == "I" || char == "o" || char == "O"
+            || char == "u" || char == "U") {
+            output.append(char)
+        }
+    }
+    return output
+}
+
+let resultVar = "Hello, World!".transform(removeVowels)
