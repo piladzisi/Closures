e670c2ebe7e07f0619298f64b846126fa8ed6bde
Anna <piladzisi@gmail.com>
2019-04-16 09:48:53 +0300

    6 rules of closure expressions syntax

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -110,6 +110,39 @@ func doubleMe(_ value: Int) -> Int {
 let doubleFunc = doubleMe
 doubleFunc(2)
 
-let numbers = [1,2,3,4]
+let numbers = [1,2,3,4,5]
 
 let doubledNumbers = numbers.map(doubleFunc)
+
+//MARK: Closure Expression Syntax
+
+// Rule #1: Define the closure inline
+
+let tripledNumbers = numbers.map({(value: Int) -> Int in
+    return value * 3
+})
+
+//Rule #2: Inferring Type from Context
+
+numbers.map({ value in return value * 3 })
+
+//Rule #3: Implicit Returns from Single-Expression Closures
+
+numbers.map({ value in value * 3 })
+
+//Rule #4: Shorthand Argument Names
+
+numbers.map({ $0 * 3 })
+
+//Rule #5: Trailing Closures
+
+numbers.map() { $0 * 3 }
+
+numbers.map() { number -> Int in
+    if number % 2 == 0 {
+        return number/2
+    }
+    return number/3
+}
+//Rule #6: Ignoring Parenthesis
+numbers.map { $0 * 3 }
